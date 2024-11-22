1614be319b7b19df052ca58343631f3610c1da48
Ángel Fdez <87870803+afg199@users.noreply.github.com>
2024-11-22 09:55:57 +0000

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,11 @@
+// Script para mostrar un modal dinámico con imágenes
 $('#ModalPhoto').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) 
-    var photo = button.data('photo') 
-    var modal = $(this)
-    modal.find('.img-fluid').attr("src", "img/" + photo + ".jpg")
- })
+    // Obtén el botón que activó el modal
+    var button = $(event.relatedTarget);
+    // Extrae el nombre de la foto desde el atributo data-photo
+    var photo = button.data('photo');
+    // Encuentra el modal actual
+    var modal = $(this);
+    // Cambia el atributo src de la imagen dentro del modal
+    modal.find('.img-fluid').attr("src", "img/" + photo + ".jpg");
+});
